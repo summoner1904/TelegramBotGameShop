ef9f6b8e55ced13175921218d3e015dd20b923da
serjasum <serjasum1994@icloud.com>
2022-11-23 10:51:04 +0500

    Добавлена проверка оплаты товара. Добавлена выдача товара в боте (Логин:Пароль).

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import telebot
 from telebot import types
 from config import TOKEN
-from qiwi import GenerateBill
+from qiwi import Bill
+
 bot = telebot.TeleBot(TOKEN)
-user = GenerateBill()
+user = Bill()
+
+
+def give_out(call, counter):
+    with open("product.txt", "r", encoding="UTF-8") as accounts:
+        account = accounts.readlines()[counter]
+    bot.send_message(call.message.chat.id, account)
+
 # Клавиатуры на все случаи жизни
 def navigation_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -46,6 +54,7 @@ def hello(message):
         "Для более удобной навигации рекомендуем использовать клавиатуру",
         reply_markup=navigation_keyboard(),
     )
+    print(message.id)
 
 
 # При нажатии на клавиатуре "Каталог"
@@ -99,28 +108,100 @@ def rules(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle(call):
-    if call.data == "gta":
-        bill_keyboard = types.InlineKeyboardMarkup()
-        button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_gta_bill())
+    def keyboard_for_pay():
+        message_id = call.message.id
+        pay_keyboard = types.InlineKeyboardMarkup()
+        if call.data == "gta":
+            button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_gta_bill(message_id))
+        elif call.data == "rust":
+            button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_rust_bill(message_id))
+        elif call.data == "rust":
+            button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_cod_bill(message_id))
+        elif call.data == "rust":
+            button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_dota_bill(message_id))
+        elif call.data == "rust":
+            button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_battlefield_bill(message_id))
+        elif call.data == "rust":
+            button_pay = types.InlineKeyboardButton("Оплатить", url=user.create_fortnite_bill(message_id))
         button_check_pay = types.InlineKeyboardButton("Проверить", callback_data="check_pay")
-        bill_keyboard.add(button_pay, button_check_pay)
+        return pay_keyboard.add(button_pay, button_check_pay)
+
+    if call.data == "gta":
         bot.send_photo(
             call.message.chat.id,
             photo="https://xage.ru/media/uploads/2018/04/gtav.jpg",
             caption="GTA V | "
             "249 "
-            "RUB\nПлатформа: Steam\nДля оплаты товара нажмите на кнопку ниже. "
-                    "У вас откроется новая вкладка, где вы сможете оплатить счёт",
-            reply_markup=bill_keyboard
+            "RUB\nПлатформа: Steam|Social Club\nДля оплаты товара нажмите на кнопку ниже. "
+            "У вас откроется новая вкладка, где вы сможете оплатить счёт",
+            reply_markup=keyboard_for_pay(),
         )
-    if call.data == "rust":
 
+    elif call.data == "rust":
+        bot.send_photo(
+            call.message.chat.id,
+            photo="https://pic.rutubelist.ru/video/48/a0/48a0ebdfc8a24d5890464bafabe79812.jpg",
+            caption="RUST | "
+            "199 "
+            "RUB\nПлатформа: Steam\nДля оплаты товара нажмите на кнопку ниже. "
+            "У вас откроется новая вкладка, где вы сможете оплатить счёт",
+            reply_markup=keyboard_for_pay(),
+        )
+
+    elif call.data == "battlefield":
+        bot.send_photo(
+            call.message.chat.id,
+            photo="http://keyplace.ru/wp-content/uploads/2019/10/bfv_theme.jpg",
+            caption="BATTLEFIELD | "
+            "699 "
+            "RUB\nПлатформа: Origin\nДля оплаты товара нажмите на кнопку ниже. "
+            "У вас откроется новая вкладка, где вы сможете оплатить счёт",
+            reply_markup=keyboard_for_pay(),
+        )
+
+    elif call.data == "cod":
+        bot.send_photo(
+            call.message.chat.id,
+            photo="https://gamebomb.ru/files/galleries/001/4/45/385757.jpg",
+            caption="CoD Vanguard | "
+            "699 "
+            "RUB\nПлатформа: Battle.net\nДля оплаты товара нажмите на кнопку ниже. "
+            "У вас откроется новая вкладка, где вы сможете оплатить счёт",
+            reply_markup=keyboard_for_pay(),
+        )
+
+    elif call.data == "dota":
+        bot.send_photo(
+            call.message.chat.id,
+            photo="https://i.postimg.cc/2y42Znym/V.png",
+            caption="Dota 2 7000 PTS | "
+            "3299 "
+            "RUB\nПлатформа: Steam\nДля оплаты товара нажмите на кнопку ниже. "
+            "У вас откроется новая вкладка, где вы сможете оплатить счёт",
+            reply_markup=keyboard_for_pay(),
+        )
+    elif call.data == "fortnite":
+        bot.send_photo(
+            call.message.chat.id,
+            photo="https://media.discordapp.net/attachments/571294092341018664/571425151338676224/unknown.png?width"
+            "=1202&height=677",
+            caption="Fortnite 200+ Skins | "
+            "1799 "
+            "RUB\nПлатформа: Epic Games\nДля оплаты товара нажмите на кнопку ниже. "
+            "У вас откроется новая вкладка, где вы сможете оплатить счёт",
+            reply_markup=keyboard_for_pay(),
+        )
     elif call.data == "check_pay":
-        user.check_gta_bill()
-        if user.check_gta_bill() == "PAID":
-            ###TODO
+        counter = 0
+        message_id = call.message.id
+        if user.check_pay(message_id):
+            bot.send_message(call.message.chat.id, f"Благодарим за покупку товара.")
+            give_out(call, counter)
+            counter += 1
         else:
-            print("Оплата пока не прошла")
+            bot.send_message(call.message.chat.id, f"Пока что оплаты не поступало.")
+
+
 
 print("Запущено")
 bot.infinity_polling()
